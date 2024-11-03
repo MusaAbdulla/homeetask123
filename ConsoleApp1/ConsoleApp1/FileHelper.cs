7ce8bda8fea26946441cae8c3504dbbc916df393
MusaAbdulla <musaa-ab108@code.edu.az>
2024-11-04 03:08:52 +0400

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/FileHelper.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class FileHelper
+    {
+        public string Path { get;  }
+        public FileHelper(string path)
+        {
+            Path = path;
+            
+        }
+        string json;
+        List<string> Files = [];
+        public void Add(string name)
+        { 
+          Files.Add(name);
+            json= JsonConvert.SerializeObject(Files);
+            using StreamWriter sw = new StreamWriter(Path,false );
+            sw.Write(json);
+        }
+        public bool Exixst(string name)
+        { 
+         return Files.Contains(name);
+        }
+        public void Update (int index , string name)
+        {
+            Files[index] = name;
+            json = JsonConvert.SerializeObject(Files);
+            using StreamWriter sw = new StreamWriter(Path, false);
+            sw.Write(json);
+        }
+        public void Delete(int index )
+        {
+        Files.RemoveAt(index);
+            json = JsonConvert.SerializeObject(Files);
+            using StreamWriter sw = new StreamWriter(Path, false);
+            sw.Write(json);
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         FileHelper fileHelper = new FileHelper(@"C:\Users\Sheki-Komp\Desktop\classhometask\ConsoleApp1\ConsoleApp1\Files\names.json");
+            fileHelper.Add("Musa");
+            fileHelper.Add("Sema");
+            fileHelper.Add("Huseynberg");
+            fileHelper.Add("Revan");
+            Console.WriteLine(fileHelper.Exixst("Musa"));
+            fileHelper.Update(2, "Kamran");
+            fileHelper.Delete(2);
+
+
+        }
+    }
+}
